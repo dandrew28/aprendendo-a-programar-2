5625341821bc680cded15797deee09389633af88
André Luiz da Fonseca Medeiros <andre_aceofbase@yahoo.com.br>
2022-08-26 11:39:08 -0300

    primeiro exercício

---
+++ tarefa-dia-3/exercicio-1.js
@@ -0,0 +1,13 @@
+// o primeiro exercício trata de CRIAR 5 variáveis que vão armazenar os seguintes valores:
+// SEU NOME
+// SEU EMAIL
+// SUA PROFISSÃO
+// SUA IDADE
+// O LUGAR QUE VOCÊ SONHA UM DIA CONHECER
+
+let nome = "André Luiz"
+let email = "andre_aceofbase@yahoo.com.br"
+let profissao = "funcionário público"
+let idade = "44 anos"
+let lugar = "Portugal"
+console.log (`meu nome é ${nome}, meu endereço de email é ${email}, trabalho como ${profissao}, tenho ${idade} de idade e gostaria de conhecer ${lugar}`)
