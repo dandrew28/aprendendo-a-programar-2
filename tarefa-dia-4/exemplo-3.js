ea773b9819838bab93b497996ebdb5127ee6a908
André Luiz da Fonseca Medeiros <andre_aceofbase@yahoo.com.br>
2022-08-26 15:02:52 -0300

    ímpares

---
+++ tarefa-dia-4/exemplo-3.js
@@ -0,0 +1,14 @@
+// criar um código começando com um número inteiro maio que 0 e exibir todos os números ímpares até chegar nesse número//
+
+
+
+let variavel = 1
+let intervalo = 10
+
+
+while (variavel <= intervalo) {
+    if(variavel % 2 == 1) {
+        console.log(variavel)}
+    variavel = variavel + 1
+}
+
