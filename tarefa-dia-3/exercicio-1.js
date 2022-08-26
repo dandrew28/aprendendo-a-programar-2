2ffb4bf3913da0847273ae45a5cf0ff7e3374aae
André Luiz da Fonseca Medeiros <andre_aceofbase@yahoo.com.br>
2022-08-26 11:48:20 -0300

    formulário

--- tarefa-dia-3/exercicio-1.js
+++ tarefa-dia-3/exercicio-1.js
@@ -8,6 +8,15 @@
 let nome = "André Luiz"
 let email = "andre_aceofbase@yahoo.com.br"
 let profissao = "funcionário público"
-let idade = "44 anos"
+let idade = 44
 let lugar = "Portugal"
 console.log (`meu nome é ${nome}, meu endereço de email é ${email}, trabalho como ${profissao}, tenho ${idade} de idade e gostaria de conhecer ${lugar}`)
+let dadosdapessoa = "Dados do usuário"
+
+console.log(dadosdapessoa)
+
+console.log(nome)
+console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(lugar)
