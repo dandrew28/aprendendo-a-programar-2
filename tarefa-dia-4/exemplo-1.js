7404d67741bacade600969c688d9b594b487c308
André Luiz da Fonseca Medeiros <andre_aceofbase@yahoo.com.br>
2022-08-26 14:17:23 -0300

    tabuada

---
+++ tarefa-dia-4/exemplo-1.js
@@ -0,0 +1,14 @@
+// trabalharemos com números de 1 até n
+// exibir todos os números de 1 até n
+// utilizar o comando while
+
+let numero = 1
+let n = 100
+
+while (numero <= n) {
+    console.log(numero)
+    numero = numero + 1
+}
+
+
+
---
+++ tarefa-dia-4/exemplo-2.js
@@ -0,0 +1,12 @@
+// fazer tabuada de 10
+
+let marcador = 1
+let m = 20
+let variavel = 100
+
+while (marcador <= m) {
+    let resultado = marcador * variavel // tem que ficar aqui dentro para que a condição while (1 atá m) seja válida.
+        
+    console.log (`${marcador} x ${variavel} = ${resultado}`)
+   marcador = marcador + 1
+}
